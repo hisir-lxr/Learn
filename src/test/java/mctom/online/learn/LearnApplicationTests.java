2a4d4daf6240a79e83ef2e6c2a7f84cf3af864b1
hisir <hisir20200909@163.com>
2023-06-28 11:08:06 +0800

    init

---
+++ src/test/java/mctom/online/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package mctom.online.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
